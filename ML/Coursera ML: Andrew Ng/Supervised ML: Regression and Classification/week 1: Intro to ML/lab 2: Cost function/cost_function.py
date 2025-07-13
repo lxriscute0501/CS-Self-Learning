2dc3f4a2f94159ce86acdd004ae38dc604b76b93
十一十一 <lixuanran798@gmail.com>
2025-07-13 22:10:32 +0800

    Update ML, course 1 week 1

---
+++ ML/Coursera ML: Andrew Ng/Supervised ML: Regression and Classification/week 1: Intro to ML/lab 2: Cost function/cost_function.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
+plt.style.use('./deeplearning.mplstyle')
+
+
+def compute_cost(x, y, w, b):
+    m = x.shape[0]
+
+    cost_sum = 0
+    for i in range(m):
+        f_wb = w * x[i] + b
+        cost = (f_wb - y[i]) ** 2
+        cost_sum = cost_sum + cost
+    total_cost = (1 / (2 * m)) * cost_sum
+
+    return total_cost
+
+
+x_train = np.array([1.0, 2.0])
+y_train = np.array([300.0, 500.0])
+
+plt_intuition(x_train,y_train)
+
+# Larger data set
+x_train = np.array([1.0, 1.7, 2.0, 2.5, 3.0, 3.2])
+y_train = np.array([250, 300, 480,  430,   630, 730,])
+
+plt.close('all')
+fig, ax, dyn_items = plt_stationary(x_train, y_train)
+updater = plt_update_onclick(fig, ax, x_train, y_train, dyn_items)
+
+# Convex cost surface
+soup_bowl()
